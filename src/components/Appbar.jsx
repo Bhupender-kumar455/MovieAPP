2a7916b7c9fb5082cd4bdfe50fff7a6d59b122b2
Bhupender kumar <kbhupender455@gmail.com>
2023-11-13 14:34:23 +0530

    withoutSearch

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -5,15 +5,39 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import "./Appbar.css";
+import { useEffect, useState } from "react";
 
 function Appbar() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState([]);
+
   return (
     <Navbar expand="lg" className="Navbar">
-      <Container>
+      <Container className="container">
         {/* brand  */}
         <Navbar.Brand className="navbarBrand" href="#home">
-          Picture<span>Verse</span>
+          <p className="mobile-text">
+            P<span>V</span>
+          </p>
+          <p className="laptop-text">
+            Picture<span>Verse</span>
+          </p>
         </Navbar.Brand>
+        <InputGroup className="inputBox">
+          <Form.Control
+            placeholder="Search.. "
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+          <Button
+            className="search-btn"
+            style={{ top: "21px" }}
+            variant="outline-secondary"
+            id="button-addon2"
+          >
+            Go!
+          </Button>
+        </InputGroup>
 
         <Navbar.Toggle
           style={{ backgroundColor: "#7147bc" }}
@@ -22,37 +46,28 @@ function Appbar() {
         <Navbar.Collapse id="basic-navbar-nav" style={{ color: "white" }}>
           <Nav className="ms-auto align-items-center">
             <Nav.Link className=" px-3">
-              <InputGroup
-                className="inputBox"
-                style={{ display: "flex", alignContent: "center" }}
-              >
-                <Form.Control
-                  placeholder="Search.. "
-                  aria-label="Recipient's username"
-                  aria-describedby="basic-addon2"
-                />
-                <Button
-                  style={{ top: "19px" }}
-                  variant="outline-secondary"
-                  id="button-addon2"
-                >
-                  Go!
-                </Button>
-              </InputGroup>
               {/* three links of Navbar  */}
             </Nav.Link>
             <Nav.Link
-              style={{ color: "#aaaaaa" }}
+              style={{ color: "#aaaaaa", fontSize: "16px" }}
               className="links  px-3"
               href="#home"
             >
-              Home
+              Watch2gether
             </Nav.Link>
-            <Nav.Link className="links px-3" href="#link">
+            <Nav.Link
+              className="links px-3 small"
+              style={{ fontSize: "16px" }}
+              href="#link"
+            >
               Latest
             </Nav.Link>
-            <Nav.Link className="links px-3" href="#link">
-              Search
+            <Nav.Link
+              className="links px-3 small"
+              style={{ fontSize: "16px" }}
+              href="#link"
+            >
+              Random
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
