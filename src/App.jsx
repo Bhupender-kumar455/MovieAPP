fa66c78c8381b883a171bec3af232711077f4a48
Bhupender kumar <kbhupender455@gmail.com>
2023-11-14 16:29:36 +0530

    homedone

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Home";
-import First from "./components/First.jsx";
+import Home from "./Home.jsx";
 function App() {
   return (
     <div>
       <Home />
-      <First />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>Welcome here</div>;
+};
+
+export default Footer;
